e511381841d9df63281d3822d7d2213d54184804
sonta2910 <sontt291099@gmail.com>
2023-04-14 14:05:55 +0700

    them city o job

--- src/main/java/com/example/casestudymodule4/controller/HomeController.java
+++ src/main/java/com/example/casestudymodule4/controller/HomeController.java
@@ -1,7 +1,17 @@
 package com.example.casestudymodule4.controller;
 
-import org.springframework.stereotype.Controller;
+import com.example.casestudymodule4.model.Company;
+import com.example.casestudymodule4.service.impl.CompanyServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/homes")
 public class HomeController {
+
+
 }
