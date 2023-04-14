7c29f86581902c3bbe6d39a8331f21a670c599dc
dinhnguyen96 <39048935+dinhnguyen96@users.noreply.github.com>
2023-04-14 14:18:27 +0700

    update

--- src/main/java/com/example/casestudymodule4/controller/AuthController.java
+++ src/main/java/com/example/casestudymodule4/controller/AuthController.java
@@ -1,6 +1,5 @@
 package com.example.casestudymodule4.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
