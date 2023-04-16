7bc8b9c1d1c2eb180141afa5633504ccf8c701e2
dinhnguyen96 <39048935+dinhnguyen96@users.noreply.github.com>
2023-04-16 13:43:38 +0700

    update

--- src/main/java/com/example/casestudymodule4/controller/HomeController.java
+++ src/main/java/com/example/casestudymodule4/controller/HomeController.java
@@ -1,16 +1,13 @@
 package com.example.casestudymodule4.controller;
 
-import com.example.casestudymodule4.model.Company;
-import com.example.casestudymodule4.service.impl.CompanyServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/homes")
-public class HomeController {
+public class HomeController
+{
 
 }
